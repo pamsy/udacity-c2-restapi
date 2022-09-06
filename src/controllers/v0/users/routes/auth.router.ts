cb6f715180e55f5d0a48588a6b10f3d28dd143f0
pamsy <pamelaorline@gmail.com>
2022-09-06 22:52:25 +0100

    eb déployé avec succès

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -36,7 +36,8 @@ function generateJWT(user: User): string {
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
     console.warn("auth.router not yet implemented, you'll cover this in lesson 5")
-    return next();
+    //return next();
+
     if (!req.headers || !req.headers.authorization){
         return res.status(401).send({ message: 'No authorization headers.' });
      }
